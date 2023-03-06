a12a1b0ae45e530aee78f701042acba7d4bf88c5
SinySs <dimakrytoi8@gmail.com>
2023-03-07 02:40:05 +0300

    Added constructors and operator []

--- include/matrix.h
+++ include/matrix.h
@@ -3,22 +3,64 @@
 //
 #pragma once
 #include <iostream>
+#include <cassert>
 
-//template<typename T>
 
 template<typename T>
 class matrix {
-    int n_col_ = 0, n_rows_ = 0;
-    T* data = nullptr;
+    int n_cols_ = 0, n_rows_ = 0;
+    T* data_ = nullptr;
+
+    struct proxy_row {
+        T* p_row;
+
+        T& operator [](int col){
+            return p_row[col - 1];
+        }
+        const T& operator [](int col) const{
+            return p_row[col - 1];
+        }
+    };
+
 
 public:
-    matrix(int col, int rows) : n_col_(col), n_rows_(rows) {};
     matrix() = default;
+    matrix(int col, int rows) : n_cols_(col), n_rows_(rows) { data_ = new T[col*rows]; };
+    matrix(int col, int rows, const T* data);
+    matrix(const matrix<T> &rhs);
+
+    int get_rows() const {return n_rows_; };
+    int get_cols() const {return n_cols_; };
 
     void print() const {
-        std::cout << "cols: " << n_col_<< " rows: " << n_rows_ << std::endl;
+        std::cout << "cols: " << n_cols_<< " rows: " << n_rows_ << std::endl;
+    }
+
+    proxy_row operator[](int row) const {
+        return proxy_row{ data_ + (row - 1) * n_cols_ };
     }
 };
+
+template<typename T>
+matrix<T>::matrix(int col, int rows, const T *data): matrix(col, rows) {
+    int num_of_elem = col * rows;
+    data_ = new T[num_of_elem];
+
+    for(int i = 0; i < num_of_elem; ++i) {
+        data_[i] = data[i];
+    }
+
+}
+
+template<typename T>
+matrix<T>::matrix(const matrix<T> &rhs): matrix(rhs.get_rows(),rhs.get_rows()) {
+    for(int i = 0; i < n_rows_; ++i) {
+        for(int j = 0; j < n_cols_; ++j) {
+            data_[i * n_cols_ + j] = rhs[i + 1][j + 1];
+        }
+    }
+}
+
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 #include "matrix.h"
+#include <vector>
 
 int main() {
-    matrix<int> m(1, 1);
+
+    double aa[] = {1, 2, 3, 4};
+    double bb[] = {1, 2, 3, 4, 5, 6, 7, 8,9};
+
+    matrix<double> m(2, 2, aa);
+    matrix<double> mm(3, 3, bb);
+
+    matrix<double> mmm = mm;
+
+    double  A = mmm[2][1];
+    std::cout <<"A:" << A;
+
+    int a = m[1][1];
+    std::cout << a << std::endl;
+    std::cout << m[1][1] << std::endl;
+
+    m[1][1] = -9;
+    std::cout << m[1][1] << std::endl;
+
+    a = mm[3][3] + m[1][1];
+    std::cout << a << std::endl;
+
+
     m.print();
 
     return 0;
